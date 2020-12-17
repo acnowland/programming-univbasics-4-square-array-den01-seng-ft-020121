406e98fb6707d57b63bc6fa231d1ef0393e0988e
Adam Nowland <acnowland@gmail.com>
2020-12-17 16:42:25 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def square_array(array)
-  # your code here
+  counter = 0 
+  square_array = []
+  while counter < array.length do
+    square_num = array[counter] * array[counter]
+    square_array.push(square_num)
+    counter += 1 
+    binding.pry
+  end
+  return square_array
 end
